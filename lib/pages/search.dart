4af45e8c8db6fec20e5e42ff7645d8c456a4cfa5
fukiko2020 <kinako.fukiko.0822@gmail.com>
2022-11-21 21:21:02 +0900

    検索画面の基本を作成

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -9,8 +9,29 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('検索条件を入力')),
-      body: const Center(
-        child: Text('検索'),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 32),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'キーワード',
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 24),
+                width: double.infinity,
+                height: 48,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('検索する'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
